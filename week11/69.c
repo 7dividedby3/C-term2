a13eee07f233944bd183c4630f0c12ab24c76b30
7dividedby3 <168267895+7dividedby3@users.noreply.github.com>
2024-05-10 21:18:31 +0800

    Create 69.c

---
+++ week11/69.c
@@ -0,0 +1,13 @@
+void FailStudent(FILE* in, FILE* out)
+{
+    char f1[13],f2[11];int f3,f4,f5;
+    while(!feof(in)){
+        fscanf(in,"%s %s %d %d %d",f1,f2,&f3,&f4,&f5);
+        if(f3<60)
+            fprintf(out,"%s %s Math\n",f1,f2);
+        if(f4<60)
+            fprintf(out,"%s %s English\n",f1,f2);
+        if(f5<60)
+            fprintf(out,"%s %s C\n",f1,f2);
+   }
+}
