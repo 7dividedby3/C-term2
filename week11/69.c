8dd097ec96f068fabb2995d6131b01b2c8f386d6
7dividedby3 <168267895+7dividedby3@users.noreply.github.com>
2024-05-11 16:15:59 +0800

    Update 69.c

--- week11/69.c
+++ week11/69.c
@@ -1,8 +1,7 @@
 void FailStudent(FILE* in, FILE* out)
 {
     char f1[13],f2[11];int f3,f4,f5;
-    while(!feof(in)){
-        fscanf(in,"%s %s %d %d %d",f1,f2,&f3,&f4,&f5);
+    while(fscanf(in,"%s %s %d %d %d",f1,f2,&f3,&f4,&f5)!=EOF){
         if(f3<60)
             fprintf(out,"%s %s Math\n",f1,f2);
         if(f4<60)
